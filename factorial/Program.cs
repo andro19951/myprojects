b9cf99d23f3dd18683ca35960886d45d634f9b7a
Andro Gvivradze <angviv@ttu.ee>
2018-02-01 22:52:03 +0200

    Second c# Program. Simple Factorial, but hey, we all have to start from somewhere

---
+++ factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int original = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Factorial(original));
+        }
+
+        static int Factorial(int original)
+        {
+            int factorial = 1;
+            for (int i = 1; i < original+1; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+        
+    }
+}
