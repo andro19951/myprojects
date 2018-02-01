5afad8dcde9bb382ec6202db1642373b2a6dc02e
Andro Gvivradze <angviv@ttu.ee>
2018-02-01 22:36:12 +0200

    IT works!!! my first c# project ON Github

---
+++ ReverseInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int original = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ReverseInteger(original));
+        }
+
+        static int ReverseInteger(int original)
+        {
+            int Reversedata=0;
+            for (int i = 0; Math.Pow(10, i) <= original; i++)
+            {
+                Console.WriteLine("I'm here suckka!");
+                Reversedata = Reversedata *10 + (original % Convert.ToInt32(Math.Pow(10, i+1))-original % Convert.ToInt32(Math.Pow(10, i)))/Convert.ToInt32(Math.Pow(10, i));
+            }
+            return Reversedata;
+        }
+    }
+}
